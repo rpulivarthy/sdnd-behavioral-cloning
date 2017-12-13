775d22e53f671e89e829472f3dd36d494d9560c0
rxp0tga <rxp0tga@LT310633.local>
2017-12-12 22:53:51 -0500

    First try

--- model.py
+++ model.py
@@ -1,18 +1,18 @@
 import csv
 import numpy as np
 import cv2
-# from keras.models import Sequential
-# from keras.layers import Flatten, Dense, Lambda
-# from keras.layers.convolutional import Convolution2D
-# from keras.layers.pooling import MaxPooling2D
+from keras.models import Sequential, Model
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, Cropping2D
+from keras.layers.pooling import MaxPooling2D
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
-def get_image(file_path):
+def get_image_path(file_path):
     filename = file_path.split('/')[-1]
-    current_path = './data/IMG/' + filename
-    return cv2.imread(current_path)
+    return './data/IMG/' + filename
 
-def load_data(left_correction, right_correction):
+def load_data(correction):
     """
     Load the data into numpy array. Load all the images; left center and right for better training.
     """
@@ -23,53 +23,107 @@ def load_data(left_correction, right_correction):
         for line in reader:
             lines.append(line)
 
-    images =[]
+    image_paths =[]
     measurements = []
 
     for line in lines:        
         measurement = float(line[3])
         for i in range(3):
-            images.append(get_image(line[i]))
+            image_paths.append(get_image_path(line[i]))
             if i == 1:
-                measurement = measurement + left_correction
+                measurement = measurement + correction
             elif i == 2:
-                measurement = measurement + right_correction
+                measurement = measurement - correction
             measurements.append(measurement)
     
-    X_train = np.array(images)
+    X_train = np.array(image_paths)
     y_train = np.array(measurements)
 
     return X_train, y_train
 
+def nVidiaModel():
+    """
+    Creates a nVidia model
+    """
+    model = Sequential()
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
+    model.add(Cropping2D(cropping=((50,20), (0,0))))
+
+    model.add(Convolution2D(24,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(36,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(48,5,5, subsample=(2,2), activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Convolution2D(64,3,3, activation='relu'))
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    return model
+
+def preprocess_image(image):
+    return cv2.cvtColor(image, cv2.COLOR_RGB2YUV)
+
+def random_flip(image, measurement):
+    if np.random.rand() < 0.5:
+        image = cv2.flip(image, 1)
+        measurement = -measurement
+    return image, measurement
  
-def generator(samples, batch_size=32):
+def generator(samples, is_training, batch_size=32):
+    """
+    Generate the required images and measurments for training/
+    `samples` is a list of pairs (`imagePath`, `measurement`).
+    """
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        samples = sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+            for imagePath, measurement in batch_samples:
+                image = cv2.imread(imagePath)
+                image = preprocess_image(image)
+                if is_training       
+                    image, measurement = random_flip(image, measurement)
+               
+                # image = cv2.cvtColor(originalImage, cv2.COLOR_BGR2RGB)
+                images.append(image)
+                angles.append(measurement)
+            inputs = np.array(images)
+            outputs = np.array(angles)
+            yield sklearn.utils.shuffle(inputs, outputs)
+
+def main():
+    image_paths, measurents = load_data(0.2)
+
+    # split samples for training and validation
+    samples = list(zip(image_paths, measurents))
+    train_samples, validation_samples = train_test_split(samples, test_size=0.2)
+
+    train_generator = generator(train_samples, True, 32)
+    validation_generator = generator(validation_samples, False, 32)
+
+    # Model Creation
+    model = nVidiaModel()
+
+    checkpoint = ModelCheckpoint('model-{epoch:03d}.h5',
+                                 monitor='val_loss',
+                                 verbose=0,
+                                 save_best_only=args.save_best_only,
+                                 mode='auto')
+
+    # Compiling and training the model
+    model.compile(loss='mse', optimizer='adam')
     
-# def build_model():
-#     """
-#         NVIDIA model
-#     """
-#     model = Sequential()
-#     model.add(Lambda(lambda x: x/127.5-1.0, input_shape=INPUT_SHAPE))
-#     model.add(Conv2D(24, 5, 5, activation='elu', subsample=(2, 2)))
-#     model.add(Conv2D(36, 5, 5, activation='elu', subsample=(2, 2)))
-#     model.add(Conv2D(48, 5, 5, activation='elu', subsample=(2, 2)))
-#     model.add(Conv2D(64, 3, 3, activation='elu'))
-#     model.add(Conv2D(64, 3, 3, activation='elu'))
-#     model.add(Dropout(args.keep_prob))
-#     model.add(Flatten())
-#     model.add(Dense(100, activation='elu'))
-#     model.add(Dense(50, activation='elu'))
-#     model.add(Dense(10, activation='elu'))
-#     model.add(Dense(1))
-#     model.summary()
-#     return model
-
-# def train_model():
-    
-#     model.compile(loss='mse', optimizer='adam')
-#     model.compile(X_train, y_train, validation_spilit = 0.2, shuffle= True, nb_epoch= 5)
-
-#     model.save('model.h5')
+    history_object = model.fit_generator(train_generator, 
+                    samples_per_epoch=len(train_samples), 
+                    validation_data=validation_generator, 
+                    nb_val_samples=len(validation_samples), 
+                    nb_epoch=3,
+                    callbacks=[checkpoint], 
+                    verbose=1)
 
 
-load_data(0.5,-0.5)
